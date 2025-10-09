24e10fc8ed7e0e4c35851c53a94041435af3ce0a
Justas Skruibys <justasskruibys@Justass-MacBook-Air.local>
2025-10-09 09:52:26 +0300

    Pradzia

---
+++ Praktine nr2/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+void Ivedimai(){
+    int n, a;
+                cout<<"Kiek buus mokiniu ?"<<endl;
+                cin>>n;
+                cout<<"Kiek bus pazymiu ?"<<endl;
+                cin>>a;
+    for (int i = 1; i < n; i++) {
+
+    }
+
+};
+
+const int Vardai =100, Pazymiai =10;
+int main() {
+int Vardai;
+    int Pazymiai;
+ string vardas [Vardai];
+int pazymiai [Vardai] [Pazymiai];
+
+int pasirinkimas;
+
+cout<<"Mokiniu pazymiu sistema"<<endl;
+cout<<"Ka norite daryti: "<<endl;
+cout<<"1 Ivesti mokiniu vardus ir pazymius"<<endl;
+cout<<"2 Perziureti pazymius"<<endl;
+cout<<"3 Atnaujinti pazymi"<<endl;
+cout<<"4 Pasalinti mokini is saraso"<<endl;
+cin >> pasirinkimas;
+    switch (pasirinkimas) {
+            case 1: {
+
+        }
+            case 2: {
+
+        }
+            case 3: {
+
+        }
+            case 4: {
+
+            }
+
+
+    }
+
+
+    return 0;
+
+}
