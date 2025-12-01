d6b01803b1dfa99b85c8a0bd4c2071beda0f35f1
justasskruibys <justasskru@gmail.com>
2025-12-01 14:23:31 +0200

    Pabaigta

--- Praktine_nr4/main.cpp
+++ Praktine_nr4/main.cpp
@@ -3,15 +3,15 @@
 #include <iomanip>
 #include <string>
 using namespace std;
-
+//Meniu patiekalo struktura
 struct menuItemType {
     string menuItem;   // patiekalo pavadinimas
     double menuPrice;  // kaina
 };
-
+//Maximalus skaicius patiekalu
 const int MAX_ITEMS = 20;
 
-// Nuskaito meniu duomenis iš failo menu.txt
+// Nuskaito meniu duomenis iš failo
 int getData(menuItemType menuList[]) {
     ifstream file("menu.txt");
     if (!file.is_open()) {
@@ -21,6 +21,7 @@ int getData(menuItemType menuList[]) {
 
 
     int count = 0;
+    //Tikrina ar failas nusiskaite
     while (!file.eof() && count < MAX_ITEMS) {
         getline(file, menuList[count].menuItem);
         file >> menuList[count].menuPrice;
@@ -34,11 +35,13 @@ int getData(menuItemType menuList[]) {
 
 // Išveda meniu
 void showMenu(menuItemType menuList[], int count) {
+    cout<<"----------------------"<<endl;
     cout << "Pusryciu meniu:"<<endl;
+    cout<<"----------------------"<<endl;
     for (int i = 0; i < count; i++) {
         cout << i + 1 << ". "<< menuList[i].menuItem<<"   "<< fixed << setprecision(2) << menuList[i].menuPrice << "e" << endl;
     }
-    cout << endl;
+    cout<<"----------------------"<<endl;
 }
 
 // Išveda sąskaitą ir įrašo į receipt.txt
@@ -50,7 +53,11 @@ void printCheck(menuItemType menuList[], int count, int quantities[]) {
     cout << fixed << setprecision(2);
 
     cout << "--- Sąskaita ---" << endl;
+
     receipt << "Saskaita"<<endl;
+    receipt<<"----------------------"<<endl;
+
+
 
     for (int i = 0; i < count; i++) {
         if (quantities[i] > 0) {
@@ -62,23 +69,24 @@ void printCheck(menuItemType menuList[], int count, int quantities[]) {
             receipt << quantities[i] << " x "<< menuList[i].menuItem <<"  "<< itemTotal << "e" << endl;
         }
     }
-
+    cout<<"----------------------"<<endl;
+    receipt<<"----------------------"<<endl;
     double tax = subtotal * 0.21;
     double total = subtotal + tax;
-cout<<endl;
+
     cout << "Mokesčiai (21%)" <<"  "<< tax << "e" << endl;
     cout << "Galutinė suma" <<"  "<< total << "e" << endl;
-
+    cout<<"----------------------"<<endl;
     receipt << "Mokesčiai (21%)" <<"  "<< tax << "e" << endl;
     receipt << "Galutinė suma" <<"  "<< total << "e" << endl;
+    receipt<<"----------------------"<<endl;
 
     receipt.close();
 }
 
-//----------------------------------------------
-// MAIN – programos pradinė dalis
-//----------------------------------------------
+// Main
 int main() {
+    //Kvieciama funkcija nuskaityti faila
     menuItemType menuList[MAX_ITEMS];
     int itemCount = getData(menuList);
 
@@ -86,18 +94,20 @@ int main() {
 
     int quantities[MAX_ITEMS] = {0};
 
-    cout << "Sveiki atvykę į restoraną" << endl;
-
+    cout << "Restoranas Kazkoks maistas" << endl;
+//Kvieciama funkcija parodanti meniu
     showMenu(menuList, itemCount);
 
     cout << "Pasirinkite patiekalus (iveskite numerį, 0 - pabaiga)"<<endl;
-
+//Patiekalu pasirinkimas
     while (true) {
         int choice;
+        cout<<"----------------------"<<endl;
         cout << "Patiekalas Nr.: ";
         cin >> choice;
 
         if (choice == 0) break;
+        //Tikrina ar egzistuoja toks pasirinkimas
         if (choice < 1 || choice > itemCount) {
             cout << "Neteisingas pasirinkimas!" << endl;
             continue;
@@ -106,7 +116,7 @@ int main() {
         int qty;
         cout << "Kiek porciju? ";
         cin >> qty;
-
+//Tikrina ar kiekis nera 0
         if (qty < 1) {
             cout << "Neteisingas kiekis!" << endl;
             continue;
@@ -115,7 +125,7 @@ int main() {
         quantities[choice - 1] += qty;
         cout << "Prideta!"<<endl;
     }
-
+//Kvieciama funkcija cekio isspauzdinimui
     printCheck(menuList, itemCount, quantities);
 
     return 0;
